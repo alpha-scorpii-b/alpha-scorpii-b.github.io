ad9cff4f0e94e1e8b603fefb463da05615d20036
urho3d-travis-ci <urho3d.travis.ci@gmail.com>
2016-02-06 01:51:16 +0000

    Travis CI: Web samples update at 2016-02-06 01:51:12 UTC.
    Commit: https://github.com/urho3d/Urho3D/commit/b528268d818e1f8d0684c6a1b5a9e0fce3826519
    
    Message: Correct copy-paste error on Matrix2 header file.
    [ci package]

--- samples/Urho3D.js
+++ samples/Urho3D.js
@@ -31,7 +31,7 @@ Module.expectedDataFileDownloads++;
                               ((Module['filePackagePrefixURL'] || '') + REMOTE_PACKAGE_BASE);
   
       var REMOTE_PACKAGE_SIZE = 11438081;
-      var PACKAGE_UUID = '4c42604c-7687-4287-81e9-8a94cdf2f32f';
+      var PACKAGE_UUID = 'aa5e1f36-3785-49e1-96d1-be4a53a7a7d7';
     
     function fetchRemotePackage(packageName, packageSize, callback, errback) {
       var xhr = new XMLHttpRequest();
