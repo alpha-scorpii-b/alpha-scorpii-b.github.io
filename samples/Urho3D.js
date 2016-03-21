a26c842fc50c27123250bc8cd80a4ec4b867bcf4
urho3d-travis-ci <urho3d.travis.ci@gmail.com>
2016-03-21 06:24:29 +0000

    Travis CI: Web samples update at 2016-03-21 06:24:24 UTC.
    Commit: https://github.com/urho3d/Urho3D/commit/41dc3142aa9402ddc9562b1cba7e187d1cd11c94
    
    Message: Fix performance issue on Emscripten build due to wrong SSE detection.
    [ci package]
    Close #1259.

--- samples/Urho3D.js
+++ samples/Urho3D.js
@@ -282,6 +282,6 @@ Module.expectedDataFileDownloads++;
   }
 
  }
- loadPackage({"files": [{"audio": 0, "start": 0, "crunched": 0, "end": 128099, "filename": "/CoreData.pak"}, {"audio": 0, "start": 128099, "crunched": 0, "end": 11429544, "filename": "/Data.pak"}], "remote_package_size": 11429544, "package_uuid": "4f3690f9-27d2-4639-a4b7-bab329457af7"});
+ loadPackage({"files": [{"audio": 0, "start": 0, "crunched": 0, "end": 128099, "filename": "/CoreData.pak"}, {"audio": 0, "start": 128099, "crunched": 0, "end": 11429544, "filename": "/Data.pak"}], "remote_package_size": 11429544, "package_uuid": "1fd27523-a96b-4a2d-87dc-c21a4d2e518a"});
 
 })();
