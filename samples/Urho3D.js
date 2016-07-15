777f1e312bc2798d0fe96a76b5c7d8142fa4a0a1
urho3d-travis-ci <urho3d.travis.ci@gmail.com>
2016-07-15 10:14:39 +0000

    Travis CI: Web samples update at 2016-07-15 10:14:33 UTC.
    Commit: https://github.com/urho3d/Urho3D/commit/761afff12282f47c06b138fd5da624322e2cf741
    
    Message: Fix dir manipulation off by one error in Android DirExists() function. Closes #1470.

--- samples/Urho3D.js
+++ samples/Urho3D.js
@@ -282,6 +282,6 @@ Module.expectedDataFileDownloads++;
   }
 
  }
- loadPackage({"files": [{"audio": 0, "start": 0, "crunched": 0, "end": 157690, "filename": "/CoreData.pak"}, {"audio": 0, "start": 157690, "crunched": 0, "end": 11450195, "filename": "/Data.pak"}], "remote_package_size": 11450195, "package_uuid": "b0514f71-2881-42a2-8e47-de7ca420e669"});
+ loadPackage({"files": [{"audio": 0, "start": 0, "crunched": 0, "end": 157658, "filename": "/CoreData.pak"}, {"audio": 0, "start": 157658, "crunched": 0, "end": 11450056, "filename": "/Data.pak"}], "remote_package_size": 11450056, "package_uuid": "41bb9060-5854-438a-9665-6ae753635d1c"});
 
 })();
