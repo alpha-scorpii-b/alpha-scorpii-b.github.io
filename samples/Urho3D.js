5ab19d738831151892fec72315a83adde43c96fe
urho3d-travis-ci <urho3d.travis.ci@gmail.com>
2017-01-17 20:48:48 +0000

    Travis CI: Web samples update at 2017-01-17 20:48:42 UTC.
    Commit: https://github.com/urho3d/Urho3D/commit/356ad88baedcdebe78f39dbbe9c7c7cbb7fd8543
    
    Message: Merge remote-tracking branch 'MonkeyFirst/PaintSelection-and-Origins'

--- samples/Urho3D.js
+++ samples/Urho3D.js
@@ -282,6 +282,6 @@ Module.expectedDataFileDownloads++;
   }
 
  }
- loadPackage({"files": [{"audio": 0, "start": 0, "crunched": 0, "end": 158076, "filename": "/CoreData.pak"}, {"audio": 0, "start": 158076, "crunched": 0, "end": 15604454, "filename": "/Data.pak"}], "remote_package_size": 15604454, "package_uuid": "89a3a74d-4379-43cb-a227-9d6181ac8b7e"});
+ loadPackage({"files": [{"audio": 0, "start": 0, "crunched": 0, "end": 158076, "filename": "/CoreData.pak"}, {"audio": 0, "start": 158076, "crunched": 0, "end": 15643951, "filename": "/Data.pak"}], "remote_package_size": 15643951, "package_uuid": "a71966e6-e3d4-4244-8df2-5c7cf15ff774"});
 
 })();
