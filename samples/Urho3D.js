56153a334f7b94df5b7d00822f44981ca62ad2b5
urho3d-travis-ci <urho3d.travis.ci@gmail.com>
2017-04-10 19:59:21 +0000

    Travis CI: Web samples update at 2017-04-10 19:59:15 UTC.
    Commit: https://github.com/urho3d/Urho3D/commit/150c4fb26da03f2dbb25fe8cbc8d1ea4448f372b
    
    Message: Export some consts to AngelScript

--- samples/Urho3D.js
+++ samples/Urho3D.js
@@ -283,6 +283,6 @@ Module.expectedDataFileDownloads++;
   }
 
  }
- loadPackage({"files": [{"audio": 0, "start": 0, "crunched": 0, "end": 158016, "filename": "/CoreData.pak"}, {"audio": 0, "start": 158016, "crunched": 0, "end": 16684419, "filename": "/Data.pak"}], "remote_package_size": 16684419, "package_uuid": "6450d37c-82ab-4e4d-8387-133d98803d67"});
+ loadPackage({"files": [{"audio": 0, "start": 0, "crunched": 0, "end": 158016, "filename": "/CoreData.pak"}, {"audio": 0, "start": 158016, "crunched": 0, "end": 16684663, "filename": "/Data.pak"}], "remote_package_size": 16684663, "package_uuid": "cc064dbd-1526-4bb3-9cfb-d286ec2ec29c"});
 
 })();
