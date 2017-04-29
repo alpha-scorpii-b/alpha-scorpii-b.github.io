2c82ca9acffe634c74efd0672a6105ef67a7fa3a
weitjong <weitjong@gmail.com>
2017-04-29 02:07:48 +0000

    Travis CI: Web samples update at 2017-04-29 02:07:43 UTC.
    Commit: https://github.com/weitjong/Urho3D/commit/a273c9cdd064ce3bde428742ad41e9cd6961f0d5
    
    Message: Sync at 2017-04-29 09:24:55 +0800.

--- samples/Urho3D.js
+++ samples/Urho3D.js
@@ -20,7 +20,7 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = '/home/travis/build/urho3d/Build/bin/Urho3D.js.data';
+    var PACKAGE_NAME = '/home/travis/build/weitjong/Build/bin/Urho3D.js.data';
     var REMOTE_PACKAGE_BASE = 'Urho3D.js.data';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
@@ -234,10 +234,10 @@ Module.expectedDataFileDownloads++;
           for (i = 0; i < files.length; ++i) {
             DataRequest.prototype.requests[files[i].filename].onload();
           }
-              Module['removeRunDependency']('datafile_/home/travis/build/urho3d/Build/bin/Urho3D.js.data');
+              Module['removeRunDependency']('datafile_/home/travis/build/weitjong/Build/bin/Urho3D.js.data');
 
     };
-    Module['addRunDependency']('datafile_/home/travis/build/urho3d/Build/bin/Urho3D.js.data');
+    Module['addRunDependency']('datafile_/home/travis/build/weitjong/Build/bin/Urho3D.js.data');
   
     if (!Module.preloadResults) Module.preloadResults = {};
   
@@ -283,6 +283,6 @@ Module.expectedDataFileDownloads++;
   }
 
  }
- loadPackage({"files": [{"audio": 0, "start": 0, "crunched": 0, "end": 157981, "filename": "/CoreData.pak"}, {"audio": 0, "start": 157981, "crunched": 0, "end": 16683672, "filename": "/Data.pak"}], "remote_package_size": 16683672, "package_uuid": "a2237f87-8e88-431f-ba57-ae4e7640a468"});
+ loadPackage({"files": [{"audio": 0, "start": 0, "crunched": 0, "end": 157981, "filename": "/CoreData.pak"}, {"audio": 0, "start": 157981, "crunched": 0, "end": 16683672, "filename": "/Data.pak"}], "remote_package_size": 16683672, "package_uuid": "5c58c4cb-3d27-4c94-8a22-b144262386b2"});
 
 })();
