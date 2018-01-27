e13da24663fac907c4729b398b996e9a9f0c9920
urho3d-travis-ci <urho3d.travis.ci@gmail.com>
2018-01-27 18:30:59 +0000

    Travis CI: Web samples update at 2018-01-27 18:30:51 UTC.
    Commit: https://github.com/urho3d/Urho3D/commit/23a696676d40c595bebbae764065e0c2f04888d1
    
    Message: Add IsReplicated() method for Node and Component classes.
    Close #2211.

--- samples/Urho3D.js
+++ samples/Urho3D.js
@@ -283,6 +283,6 @@ Module.expectedDataFileDownloads++;
   }
 
  }
- loadPackage({"files": [{"audio": 0, "start": 0, "crunched": 0, "end": 154658, "filename": "/CoreData.pak"}, {"audio": 0, "start": 154658, "crunched": 0, "end": 17511407, "filename": "/Data.pak"}], "remote_package_size": 17511407, "package_uuid": "3d079c61-03bb-4184-8432-5ce7d108b835"});
+ loadPackage({"files": [{"audio": 0, "start": 0, "crunched": 0, "end": 154658, "filename": "/CoreData.pak"}, {"audio": 0, "start": 154658, "crunched": 0, "end": 17511195, "filename": "/Data.pak"}], "remote_package_size": 17511195, "package_uuid": "002462b2-37a8-46a0-97e3-c2bc795ae26c"});
 
 })();
