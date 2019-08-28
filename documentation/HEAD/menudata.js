d034c0ce414b3bebd696c7fff17d6808f337a223
urho3d-travis-ci <urho3d.travis.ci@gmail.com>
2019-08-28 15:04:40 +0000

    Travis CI: site documentation update at 2019-08-28 15:04:38 UTC.
    Commit: https://github.com/urho3d/Urho3D/commit/06fa2a728dc9c7dfa2ab04aef8b7f0ec5d0ff2c2
    
    Message: Make doxygen catch global enums

--- documentation/HEAD/menudata.js
+++ documentation/HEAD/menudata.js
@@ -96,4 +96,11 @@ var menudata={children:[
 {text:"z",url:"functions_vars_z.html#index_z"}]},
 {text:"Typedefs",url:"functions_type.html"},
 {text:"Enumerations",url:"functions_enum.html"},
-{text:"Enumerator",url:"functions_eval.html"}]}]}]}
+{text:"Enumerator",url:"functions_eval.html"}]}]},
+{text:"Files",url:"files.html",children:[
+{text:"File List",url:"files.html"},
+{text:"File Members",url:"globals.html",children:[
+{text:"All",url:"globals.html"},
+{text:"Enumerations",url:"globals_enum.html"},
+{text:"Enumerator",url:"globals_eval.html"},
+{text:"Macros",url:"globals_defs.html"}]}]}]}
