050f4e128caf110d22a23cc501a3c4996ecb0719
urho3d-travis-ci <urho3d.travis.ci@gmail.com>
2020-03-16 03:02:23 +0000

    Travis CI: Web samples update at 2020-03-16 03:02:22 UTC.
    Commit: https://github.com/urho3d/Urho3D/commit/96cb0d5d1eda60109563980e5f630c7af7477e39
    
    Message: Fixed event data names in sample 25
    
    Liberates emitter from screen corner.

--- samples/Urho3D.js
+++ samples/Urho3D.js
@@ -337,7 +337,7 @@
     }
   
    }
-   loadPackage({"files": [{"filename": "/CoreData.pak", "start": 0, "end": 154658, "audio": 0}, {"filename": "/Data.pak", "start": 154658, "end": 17533725, "audio": 0}], "remote_package_size": 17533725, "package_uuid": "ed774678-4717-4dd2-9649-be94cba98ef2"});
+   loadPackage({"files": [{"filename": "/CoreData.pak", "start": 0, "end": 154658, "audio": 0}, {"filename": "/Data.pak", "start": 154658, "end": 17533725, "audio": 0}], "remote_package_size": 17533725, "package_uuid": "2377d936-1f08-47f1-b397-e5a23400e61a"});
   
   })();
   
