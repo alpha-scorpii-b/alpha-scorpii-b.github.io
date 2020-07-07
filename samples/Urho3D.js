a479beb9bb1c8fac6a96de3be4e05b02f4ae3c00
urho3d-travis-ci <urho3d.travis.ci@gmail.com>
2020-07-07 13:05:57 +0000

    Travis CI: Web samples update at 2020-07-07 13:05:56 UTC.
    Commit: https://github.com/urho3d/Urho3D/commit/b16935c834a977c829bc4556c807ecb6ca64013a
    
    Message: Update authors

--- samples/Urho3D.js
+++ samples/Urho3D.js
@@ -337,7 +337,7 @@
     }
   
    }
-   loadPackage({"files": [{"filename": "/CoreData.pak", "start": 0, "end": 168833, "audio": 0}, {"filename": "/Data.pak", "start": 168833, "end": 17548352, "audio": 0}], "remote_package_size": 17548352, "package_uuid": "e497e3f1-3ffe-4d0c-a13d-2ab54f2774be"});
+   loadPackage({"files": [{"filename": "/CoreData.pak", "start": 0, "end": 168905, "audio": 0}, {"filename": "/Data.pak", "start": 168905, "end": 17548424, "audio": 0}], "remote_package_size": 17548424, "package_uuid": "0f13852d-914d-412c-84be-8b787be04106"});
   
   })();
   
