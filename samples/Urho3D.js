9d52389578b82055bf62573c5b0a1bd5b01b93ed
urhobot <weitjong+urhobot@gmail.com>
2021-07-04 18:46:08 +0800

    GH Actions: Web samples update at 2021-07-04 10:46:06 UTC.
    Commit: https://github.com/urho3d/Urho3D/commit/a4ea40bff0e3110b2bddd958d2b7bf18cf310a1c
    
    Message: Set default staged install dir for rake install task.

--- samples/Urho3D.js
+++ samples/Urho3D.js
@@ -341,7 +341,7 @@
     }
   
    }
-   loadPackage({"files": [{"filename": "/CoreData.pak", "start": 0, "end": 176615, "audio": 0}, {"filename": "/Data.pak", "start": 176615, "end": 17557430, "audio": 0}], "remote_package_size": 17557430, "package_uuid": "752e5812-6e12-4ade-b2ee-d243ec13d2ce"});
+   loadPackage({"files": [{"filename": "/CoreData.pak", "start": 0, "end": 176615, "audio": 0}, {"filename": "/Data.pak", "start": 176615, "end": 17557430, "audio": 0}], "remote_package_size": 17557430, "package_uuid": "6fdb1182-5824-4acc-aba2-92103f6988dc"});
   
   })();
   
