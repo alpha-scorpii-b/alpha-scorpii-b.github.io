f4883b3ac7273b5deb85e2bf5673ab27ce5c91b3
urhobot <weitjong+urhobot@gmail.com>
2021-12-22 07:24:00 +0800

    GH Actions: Web samples update at 2021-12-21 23:23:58 UTC.
    Commit: https://github.com/urho3d/Urho3D/commit/b159ae72b29cf8198fa966a3352659bd7cc50383
    
    Message: Fixed RibbonTrail documentation line that called color scale. (#2856)

--- samples/Urho3D.js
+++ samples/Urho3D.js
@@ -341,7 +341,7 @@
     }
   
    }
-   loadPackage({"files": [{"filename": "/CoreData.pak", "start": 0, "end": 176615, "audio": 0}, {"filename": "/Data.pak", "start": 176615, "end": 17557441, "audio": 0}], "remote_package_size": 17557441, "package_uuid": "645bf361-112d-4e1e-84d6-9971a51532d9"});
+   loadPackage({"files": [{"filename": "/CoreData.pak", "start": 0, "end": 176615, "audio": 0}, {"filename": "/Data.pak", "start": 176615, "end": 17557441, "audio": 0}], "remote_package_size": 17557441, "package_uuid": "869be6f1-70b3-457f-a28c-6aa49db646a0"});
   
   })();
   
