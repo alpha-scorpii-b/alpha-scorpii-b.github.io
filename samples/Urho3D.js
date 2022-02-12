8cecb388a21d6c4c057f342490b9acb938e268c0
urhobot <weitjong+urhobot@gmail.com>
2022-02-12 21:03:08 +0800

    GH Actions: Web samples update at 2022-02-12 13:03:06 UTC.
    Commit: https://github.com/urho3d/Urho3D/commit/8014f1b111664a4bc6b09928bd8230de0b9362f5
    
    Message: 99_Benchmark: use standart way for source grouping (#2875)

--- samples/Urho3D.js
+++ samples/Urho3D.js
@@ -341,7 +341,7 @@
     }
   
    }
-   loadPackage({"files": [{"filename": "/CoreData.pak", "start": 0, "end": 176615, "audio": 0}, {"filename": "/Data.pak", "start": 176615, "end": 17557449, "audio": 0}], "remote_package_size": 17557449, "package_uuid": "1541780d-0293-45ff-976f-c1e0051df73b"});
+   loadPackage({"files": [{"filename": "/CoreData.pak", "start": 0, "end": 176615, "audio": 0}, {"filename": "/Data.pak", "start": 176615, "end": 17615413, "audio": 0}], "remote_package_size": 17615413, "package_uuid": "578be4e7-7b2c-44ef-a2e6-35fd042a922d"});
   
   })();
   
