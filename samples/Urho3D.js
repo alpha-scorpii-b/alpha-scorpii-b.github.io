e0096fc15e4e1a9c59633b51f501326804a3b0d0
urhobot <weitjong+urhobot@gmail.com>
2022-05-13 05:38:10 +0800

    GH Actions: Web samples update at 2022-05-12 21:38:08 UTC.
    Commit: https://github.com/urho3d/Urho3D/commit/78ecde1a43da13314da5e4eada8f91bc4c03007e
    
    Message: AS bindings for BigInt | Tests for AS bindings

--- samples/Urho3D.js
+++ samples/Urho3D.js
@@ -341,7 +341,7 @@
     }
   
    }
-   loadPackage({"files": [{"filename": "/CoreData.pak", "start": 0, "end": 176752, "audio": 0}, {"filename": "/Data.pak", "start": 176752, "end": 17617075, "audio": 0}], "remote_package_size": 17617075, "package_uuid": "13b1bcb3-ea75-4116-a750-4e2cc07c9154"});
+   loadPackage({"files": [{"filename": "/CoreData.pak", "start": 0, "end": 176752, "audio": 0}, {"filename": "/Data.pak", "start": 176752, "end": 17617650, "audio": 0}], "remote_package_size": 17617650, "package_uuid": "cbc5d5d9-451f-4882-8f15-75d3170e90d7"});
   
   })();
   
